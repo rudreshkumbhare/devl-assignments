2c11064f84677ff99e3a43f100b05a2b7d773a99
rudresh <rudreshkumbhare28@gmail.com>
2026-09-07 20:41:16 +0530

    Add Assignment 2

---
+++ assignment-2/2a.py
@@ -0,0 +1,60 @@
+import pandas as pd
+import altair as alt
+
+column_names = [
+    "sepal_length",
+    "sepal_width",
+    "petal_length",
+    "petal_width",
+    "species"
+]
+
+df = pd.read_csv("iris.data", names=column_names)
+
+print("Complete DataFrame")
+print(df.to_string())
+
+print("\nFirst 5 Rows")
+print(df.head())
+
+print("\nLast 5 Rows")
+print(df.tail())
+
+print("\nDataFrame Information")
+print(df.info())
+
+print("\nShape of DataFrame")
+print(df.shape)
+
+print("\nStatistical Summary")
+print(df.describe())
+
+print("\nUnique Species")
+print(df["species"].unique())
+
+print("\nSpecies Count")
+print(df["species"].value_counts())
+
+# Access row using iloc
+print("\nRow at Index 10")
+print(df.iloc[10])
+
+# Access specific value
+print("\nValue at Row 0, Column 2")
+print(df.iloc[0, 2])
+
+# Access using loc
+print("\nRow using loc")
+print(df.loc[0])
+
+# Remove missing values
+df = df.dropna()
+
+print("\nAverage Measurements by Species")
+summary_by_group = df.groupby("species").mean()
+print(summary_by_group)
+
+df.to_csv("iris_cleaned.csv", index=False)
+summary_by_group.to_json("summary.json")
+
+print("\nFiles Saved Successfully!")
---
+++ assignment-2/2b.py
@@ -0,0 +1,59 @@
+import pandas as pd
+from sklearn.preprocessing import LabelEncoder, OneHotEncoder
+
+df = pd.read_csv("/content/synthetic_dataset.csv")
+
+print("Raw data")
+print(df.head(10))
+
+print("\nReplace Empty Set")
+mean_filling = df["Rating"].fillna(df["Rating"].mean())
+mode_filling = df["Stock"].fillna(df["Stock"].mode().iloc[0])
+median_filling = df["Discount"].fillna(df["Discount"].median())
+df["Rating"] = mean_filling
+df["Stock"] = mode_filling
+df["Discount"] = median_filling
+
+median_filling = df["Price"].fillna(df["Price"].median())
+mode_filling = df["Category"].fillna(df["Category"].mode().iloc[0])
+df["Price"] = median_filling
+df["Category"] = mode_filling
+
+print(df.head(10))
+
+print("\nWrong Data")
+for x in df.index:
+    if df.loc[x, "Discount"] > 30:
+        df.loc[x, "Discount"] = 25
+print(df.head(10))
+
+print("\nNull Values")
+nulls_column = df.isnull().sum()
+print(nulls_column)
+
+df.duplicated()
+df.drop_duplicates(inplace=True)
+
+print("\nInterquartile Range (IQR)")
+print(df.describe())
+
+Q1 = df.Price.quantile(0.25)
+Q3 = df.Price.quantile(0.75)
+IQR = Q3 - Q1
+print("IQR :", IQR)
+lower_limit = Q1 - (1.5 * IQR)
+upper_limit = Q3 + (1.5 * IQR)
+
+print("Lower Limit :", lower_limit)
+print("Upper Limit :", upper_limit)
+
+print("\nLabel Encoder")
+le = LabelEncoder()
+df["Category"] = le.fit_transform(df["Category"])
+print(df.head())
+
+print("\nOne Hot Encoder")
+one = OneHotEncoder(sparse_output=False)
+Stock = ["In Stock", "Stock"]
+df["Stock"] = one.fit_transform(df[["Stock"]])
+print(df.head())
